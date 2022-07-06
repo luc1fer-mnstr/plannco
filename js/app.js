3d7f0a22a4dd14e51b9ffcf506f25643d0a07757
Kay-007 <waieainmin@gmail.com>
2022-07-07 01:36:02 +0630

    7 July 2022

---
+++ js/app.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+
+	// Start Header
+
+		// Start Nav
+
+		$(".navbuttons").click(function(){
+			$(this).toggleClass("crossxs");
+		});
+
+		// End Nav
+
+		// for nav
+		$(window).scroll(function(){
+			let getscrolltop = $(this).scrollTop();
+			// console.log(getscrolltop);
+
+			if(getscrolltop >= 200){
+				$(".navbar").addClass("navmenus");
+			}else{
+				$(".navbar").removeClass("navmenus");
+			}
+		});
+
+	// End Header
+
+	// Start Properties
+
+	$(".propertylists").click(function(){
+
+		// for activeitems
+
+		// $(this).addClass("activeitems");
+		// $(this).siblings().removeClass("activeitems");
+		$(this).addClass("activeitems").siblings().removeClass("activeitems");
+
+
+		// for filter
+
+		let getattvalue = $(this).attr("data-filter");
+		// console.log(getattvalue);
+
+		if(getattvalue === "all"){
+			$(".filters").show("explode",1000);
+		}else{
+			$(".filters").hide();
+			$(".filters").not("."+getattvalue).hide("explode",1000);
+			$(".filters").filter("."+getattvalue).show("explode",1000);
+		}
+
+	});
+
+	// For image overlay (or) lightbox2
+	lightbox.option({
+		showImageNumberLabel:false
+	})
+
+	// End Properties
+
+
+
+	// Start Adv Section 
+
+	$(window).scroll(function(){
+
+		let getscrolltop = $(this).scrollTop();
+		// console.log(getscrolltop);
+
+		if(getscrolltop >= 900){
+			$(".advimages").addClass("fromlefts");
+			$(".advtexts").addClass("fromrights");
+		}else{
+			$(".advimages").removeClass("fromlefts");
+			$(".advtexts").removeClass("fromrights");
+		}
+
+	});
+
+	// End Adv Section
+
+});
